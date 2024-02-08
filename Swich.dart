8240dd2d91480fa8811d7518bb85701f1b9a62d7
Abrar Jihad <jihad.202002082@green.ac.bd>
2024-02-09 00:48:39 +0600

    Add files via upload

---
+++ Swich.dart
@@ -0,0 +1,14 @@
+void main() {
+  var num = 2;
+  switch (num) {
+    case 1:
+      print("Value is 1");
+      break;
+    case 2:
+      print("Value is 2");
+      break;
+    case 3:
+      print("value is 3");
+      break;
+  }
+}
---
+++ ternary.dart
@@ -0,0 +1,4 @@
+void main() {
+  var num = 100;
+  num > 110 ? print('Its Ture') : print('Its false');
+}
